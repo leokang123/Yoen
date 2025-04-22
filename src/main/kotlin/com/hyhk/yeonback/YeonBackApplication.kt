1f3f2114af71b1b643ab8391ec48077ac695c123
leokang123 <leokang123@github.com>
2025-04-22 19:22:48 +0900

    First Commit

---
+++ src/main/kotlin/com/hyhk/yeonback/YeonBackApplication.kt
@@ -0,0 +1,11 @@
+package com.hyhk.yeonback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YeonBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<YeonBackApplication>(*args)
+}
